f8991e15d0282fae62a5cf50b80edad5071eab25
weijing <waiging.lau@gmail.com>
2013-08-09 12:52:00 -0400

    Create RWLock.hpp

---
+++ RWLock.hpp
@@ -0,0 +1,29 @@
+#ifndef RWLOCK_HPP
+#define RWLOCK_HPP
+
+
+#include<mutex>
+#include<condition_variable>
+
+
+class RWLock {
+public:
+    RWLock();
+    ~RWLock();
+
+
+    void acquireReader();
+    void releaseReader();
+
+
+    void acquireWriter();
+    void releaseWriter();
+
+
+private:
+    int numReaders_;
+    std::mutex mtx_numReaders_;
+    std::condition_variable_any cv_numReaders_;
+};
+
+#endif
